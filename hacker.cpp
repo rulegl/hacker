d6f3eba5ecc6a517e157caf519caa36ade494dbb
rulegl <ruler_kafei@hotmail.com>
2016-12-15 03:38:46 +0000

    first commit

---
+++ hacker.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : hacker.cpp
+// Author      : liang_guo
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
